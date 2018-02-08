f814f0073a199a9d8c5403847165ae177ea439fd
Kyle Begovich <begovichkyle@gmail.com>
2018-02-08 20:43:59 +0000

    added code from section 1.3 & doing 1.4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Hello, World!\n
+    ¡hola, mundo!"
+  end
+  
+  def goodbye
+    render html: "Goodbye (not so) cruel world!"
+  end
+
 end
